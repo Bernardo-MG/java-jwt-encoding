d365a28cd470f28d1f0c7ca423ccbd6ffd86477d
Bernardo <programming@bernardomg.com>
2026-07-17 23:50:33 +0200

    Initial structure

---
+++ src/main/java/com/bernardomg/framework/jwt/MainClass.java
@@ -0,0 +1,20 @@
+package com.bernardomg.framework.jwt;
+
+/**
+ * Placeholder entry point for the Java JWT library.
+ */
+public final class MainClass {
+
+    private MainClass() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * Returns the generated project name.
+     *
+     * @return the project name
+     */
+    public static String projectName() {
+        return "Java JWT";
+    }
+}
